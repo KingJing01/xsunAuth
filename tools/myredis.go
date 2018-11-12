78b664f76f77a523961e084584d9e475dff52d6b
KingJing01 <18049808631@163.com>
2018-11-12 16:45:30 +0800

    add redis.go

---
+++ tools/myredis.go
@@ -0,0 +1,27 @@
+package tools
+
+import (
+	"log"
+	"time"
+
+	redis "github.com/chasex/redis-go-cluster"
+)
+
+var Globalcluster *redis.Cluster
+
+//初始化 redis
+func InitRedis() {
+	cluster, err := redis.NewCluster(
+		&redis.Options{
+			StartNodes:   []string{"10.10.10.208:7010", "10.10.10.208:7011", "10.10.10.197:7012"},
+			ConnTimeout:  50 * time.Millisecond,
+			ReadTimeout:  50 * time.Millisecond,
+			WriteTimeout: 50 * time.Millisecond,
+			KeepAlive:    16,
+			AliveTime:    60 * time.Second,
+		})
+	if err != nil {
+		log.Fatalf("redis.New error: %s", err.Error())
+	}
+	Globalcluster = cluster
+}
